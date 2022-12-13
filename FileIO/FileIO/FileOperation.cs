6f12fa62c7fdc428161121b65f20d49e2b2de46e
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-13 09:10:23 +0530

    [Suraj]Add:Checking File Exist Or Not

---
+++ FileIO/FileIO/FileOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    public class FileOperation
+    {
+        string filePath = @"C:\Users\suraj\source\repos\217 .Net\File-IO-Operation\FileIO\FileIO\Operation.txt";
+        public void FileExist() 
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("Given File Exists");
+            }
+            else
+            {
+                Console.WriteLine("File Does not Exists");
+            }
+        }
+    }
+}
---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FileIO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FileOperation fileOperation = new FileOperation();
+            Console.WriteLine("Welcome To File Operation Methods");
+            Console.WriteLine("Select Below Option");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine(" 1. Check Files Exists or Not");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        fileOperation.FileExist();
+                        break;
+                    default:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
